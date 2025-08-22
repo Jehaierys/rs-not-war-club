3c8cf7d316fcef236b21002b53161abc0b7630d4
Jehaierys <kawaysempai@gmail.com>
2025-08-22 18:08:58 +0500

    fix: almost everything is done!

---
+++ script/MessageBuilder.js
@@ -0,0 +1,33 @@
+class MessageBuilder {
+    #message = '';
+
+    static builder() {
+        return new MessageBuilder();
+    }
+
+    attacking(heroName) {
+        this.#message += heroName;
+        return this;
+    }
+
+    attackFlow(successful, type) {
+        this.#message += defaultMessageTemplate.template(successful, type);
+        return this;
+    }
+
+    defending(heroName) {
+        this.#message += heroName;
+        return this;
+    }
+
+    damage(damage) {
+        if (damage > 0) {
+            this.#message += `, нанесено ${damage} ед. урона`;
+        }
+        return this;
+    }
+
+    build() {
+        return this.#message;
+    }
+}
